5538b3375ec208c61aa0f899790f3b5724b32b7d
MokhammadVu <stechkin_95@hotmail.com>
2023-06-23 11:26:45 +0200

    upp

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/router-task",
+  base: "/router-task-yu",
   plugins: [react()],
 });
